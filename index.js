63c4ad0110d776132b40eba094894c95e30bf1de
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-03 12:43:17 -0500

    commit tree 786d77f637343eaf990f7427e1282bd2f7d95897

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driversNames,name) {
+    const nameUpper = name.toUpperCase();
+    return driversNames.filter(driver => driver.toUpperCase() === nameUpper);
+}
+
+function fuzzyMatch(names,letters) {
+    return names.filter(name=>name.slice(0,letters.length)===letters);
+}
+
+function matchName(drivers,string) {
+    return drivers.filter(driver=>driver.name===string);
+}
